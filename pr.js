9412fa3b5870e87718de1a39b4fd74a57abcaa2d
ptds106 <ptds106@gmail.com>
2020-01-31 14:29:18 -0800

    maerong

--- pr.js
+++ pr.js
@@ -1,3 +1,12 @@
-group = ["Paul", "Bita", "Blake", "Joshua"]
+function group() {
+    group = ["Paul", "Bita", "Blake", "Joshua"]
+    group.forEach((g, idx) => {
+        if (g === "Joshua") {
+            return (g + "is the manager of this group")
+        } else {
+            return (g + 'is member of this group')
+        }
+    })
 
-console.log(group)
+}
+group();
